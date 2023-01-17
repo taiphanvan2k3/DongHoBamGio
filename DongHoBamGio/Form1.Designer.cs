ec2a710f0faf90a923f29509bdcf5ece6fda4beb
taiphanvan2k3 <taiphanvan2403@gmail.com>
2023-01-17 09:43:03 +0700

    Add project files.

---
+++ DongHoBamGio/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace DongHoBamGio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelShowTime = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.btnStartPause = new System.Windows.Forms.Button();
+            this.labelTick = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelShowTime
+            // 
+            this.labelShowTime.BackColor = System.Drawing.SystemColors.ControlText;
+            this.labelShowTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelShowTime.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelShowTime.Location = new System.Drawing.Point(141, 58);
+            this.labelShowTime.Name = "labelShowTime";
+            this.labelShowTime.Size = new System.Drawing.Size(518, 156);
+            this.labelShowTime.TabIndex = 0;
+            this.labelShowTime.Text = "00 : 00 : 00";
+            this.labelShowTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.BackColor = System.Drawing.Color.Gray;
+            this.btnRestart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRestart.Location = new System.Drawing.Point(428, 256);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(159, 50);
+            this.btnRestart.TabIndex = 2;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = false;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // btnStartPause
+            // 
+            this.btnStartPause.BackColor = System.Drawing.Color.Green;
+            this.btnStartPause.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStartPause.Location = new System.Drawing.Point(205, 256);
+            this.btnStartPause.Name = "btnStartPause";
+            this.btnStartPause.Size = new System.Drawing.Size(159, 50);
+            this.btnStartPause.TabIndex = 1;
+            this.btnStartPause.Text = "Start";
+            this.btnStartPause.UseVisualStyleBackColor = false;
+            this.btnStartPause.Click += new System.EventHandler(this.btnStartPause_Click);
+            // 
+            // labelTick
+            // 
+            this.labelTick.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.labelTick.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTick.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelTick.Location = new System.Drawing.Point(470, 126);
+            this.labelTick.Name = "labelTick";
+            this.labelTick.Size = new System.Drawing.Size(64, 23);
+            this.labelTick.TabIndex = 3;
+            this.labelTick.Text = "00";
+            this.labelTick.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelTick);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnStartPause);
+            this.Controls.Add(this.labelShowTime);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelShowTime;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Button btnStartPause;
+        private System.Windows.Forms.Label labelTick;
+    }
+}
+
---
+++ DongHoBamGio/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DongHoBamGio
+{
+    public partial class Form1 : Form
+    {
+        private int hours, minutes, seconds, tick;
+        private Thread t;
+        private void runTime()
+        {
+            tick++;
+            if (tick < 10)
+            {
+                labelTick.Text = "0" + tick;
+            }
+            else
+            {
+                labelTick.Text = tick+"";
+            }
+            if (tick == 100)
+            {
+                tick = 0;
+                seconds++;
+                if (seconds >= 60)
+                {
+                    seconds = 0;
+                    minutes++;
+                    if (minutes >= 60)
+                    {
+                        hours++;
+                        minutes = 0;
+                    }
+                }
+            }
+            labelShowTime.Text = this.showTime();
+        }
+
+        private string showTime()
+        {
+            string res = "";
+            if (hours < 10)
+                res += "0";
+            res += hours + ":";
+            if (minutes < 10)
+                res += "0";
+            res += minutes + ":";
+            if (seconds < 10)
+                res += "0";
+            res += seconds;
+            return res;
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            if (btnRestart.Text == "Đặt lại")
+            {
+                hours = minutes = seconds = tick = 0;
+                labelShowTime.Text = showTime();
+                labelTick.Text = "00";
+            }
+        }
+
+        public Form1()
+        {
+            hours = minutes = seconds = tick = 0;
+            t = new Thread(() =>
+            {
+                while (true)
+                {
+                    //Tuy 1s =100 tick => Sleep 10ms nhưng để trừ hao do tính toán nên dùng 6ms
+                    Thread.Sleep(6);
+                    runTime();
+                }
+            });
+            t.IsBackground = true;
+            InitializeComponent();
+        }
+
+        private void btnStartPause_Click(object sender, EventArgs e)
+        {
+            if (btnStartPause.Text == "Start")
+            {
+                t.Start();
+                btnStartPause.Text = "Pause";
+                btnStartPause.BackColor = Color.Red;
+                btnRestart.Text = "Bấm";
+            }
+            else if (btnStartPause.Text == "Pause")
+            {
+                //Tạm dừng thread
+                t.Suspend();
+                btnStartPause.Text = "Continue";
+                btnStartPause.BackColor = Color.Purple;
+                this.btnRestart.Text = "Đặt lại";
+            }
+            else if (btnStartPause.Text == "Continue")
+            {
+                //Cho thread chạy lại
+                t.Resume();
+                btnStartPause.Text = "Pause";
+                btnStartPause.BackColor = Color.Red;
+                this.btnRestart.Text = "Bấm";
+            }
+        }
+    }
+}
